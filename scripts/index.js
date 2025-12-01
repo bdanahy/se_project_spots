8cc74c47a3fdd9d3d3e1bc5b36ec4a04de69cf9c
Brendan <brendan.danahy@gmail.com>
2025-11-30 19:55:46 -0700

    add delete button functionality and image preview modal; update card and modal styles

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,9 @@
 const initialCards = [
   {
+    name: "Golden Gate Bridge",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/7-photo-by-griffin-wooldridge-from-pexels.jpg",
+  },
+  {
     name: "Val Thorens",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
   },
@@ -36,6 +40,24 @@ const profileDescriptionInput = profileEditModal.querySelector(
   "#profile-description-input"
 );
 
+// select new post modal and elements
+const newPostBtn = document.querySelector(".profile__add-btn");
+const newPostModal = document.querySelector("#new-post-modal");
+const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
+const newPostFormElement = newPostModal.querySelector(".modal__form");
+const newPostNameInput = newPostModal.querySelector("#card-caption-input");
+const newPostLinkInput = newPostModal.querySelector("#card-image-input");
+
+// select image preview modal elements
+const previewModal = document.querySelector("#image-preview-modal");
+const previewImage = previewModal.querySelector(".modal__image");
+const previewCaption = previewModal.querySelector(".modal__caption");
+const previewModalImageContainer = previewModal.querySelector(
+  ".modal__image-container"
+);
+const previewCloseBtn =
+  previewModalImageContainer.querySelector(".modal__close-btn");
+
 // select profile text elements
 const profileNameElement = document.querySelector(".profile__name");
 const profileDescriptionElement = document.querySelector(
@@ -56,15 +78,27 @@ function getCardElement(data) {
   cardImageElement.alt = data.name;
   cardTitleElement.textContent = data.name;
 
+  cardImageElement.addEventListener("click", () => {
+    previewImage.src = data.link;
+    previewImage.alt = data.name;
+    previewCaption.textContent = data.name;
+    openModal(previewModal);
+  });
+
   const cardLikeBtnElement = cardElement.querySelector(".card__like-btn");
   cardLikeBtnElement.addEventListener("click", () => {
     cardLikeBtnElement.classList.toggle("card__like-btn_active");
   });
 
+  const cardDelBtnElement = cardElement.querySelector(".card__del-btn");
+  cardDelBtnElement.addEventListener("click", () => {
+    cardDelBtnElement.closest(".card").remove();
+  });
+
   return cardElement;
 }
 
-// Utility functions
+// reusable functions
 function openModal(modalElement) {
   modalElement.classList.add("modal_is-opened");
 }
@@ -95,13 +129,10 @@ profileModalCloseBtn.addEventListener("click", function () {
   closeModal(profileEditModal);
 });
 
-// select new post modal and elements
-const newPostBtn = document.querySelector(".profile__add-btn");
-const newPostModal = document.querySelector("#new-post-modal");
-const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
-const newPostFormElement = newPostModal.querySelector(".modal__form");
-const newPostNameInput = newPostModal.querySelector("#card-caption-input");
-const newPostLinkInput = newPostModal.querySelector("#card-image-input");
+// close preview modal
+previewCloseBtn.addEventListener("click", () => {
+  closeModal(previewModal);
+});
 
 // submit new post
 function handleNewPostSubmit(evt) {
